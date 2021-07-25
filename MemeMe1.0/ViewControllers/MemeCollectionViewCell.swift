0ef77af2c7119f37b2f99f640e6414bc5a5a3157
Jameka Echols <jameka.echols@gmail.com>
2021-07-25 17:34:53 -0500

    added memecollectionviewcell

---
+++ MemeMe1.0/ViewControllers/MemeCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMe1.0
+//
+//  Created by Jameka Echols on 7/25/21.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+}
