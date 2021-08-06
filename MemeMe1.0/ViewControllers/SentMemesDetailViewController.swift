bb229e8f70eef4881345542420f8662ed32c85bb
Jameka Echols <jameka.echols@gmail.com>
2021-08-05 21:24:01 -0500

    added detail view controller for the table view controller

---
+++ MemeMe1.0/ViewControllers/SentMemesDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SentMemesDetailViewController.swift
+//  MemeMe1.0
+//
+//  Created by Jameka Echols on 8/5/21.
+//
+
+import UIKit
+
+class SentMemesDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
