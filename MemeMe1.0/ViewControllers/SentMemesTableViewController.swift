cbd951d01cb96457d29d14697fc7e1fc8ed4fef6
Jameka Echols <jameka.echols@gmail.com>
2021-08-05 21:18:44 -0500

    added the cell identifier

---
+++ MemeMe1.0/ViewControllers/SentMemesTableViewController.swift
@@ -0,0 +1,62 @@
+//
+//  SentMemesTableViewController.swift
+//  MemeMe1.0
+//
+//  Created by Jameka Echols on 8/5/21.
+//
+
+import UIKit
+
+class SentMemesTableViewController: UITableViewController {
+    
+    
+    
+    var memes: [Meme]!{
+        let object = UIApplication.shared.delegate
+        let appDelegate = object as! AppDelegate
+        return appDelegate.memes
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return memes.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MemeCell", for: indexPath)
+        let meme = self.memes[(indexPath as NSIndexPath).row]
+        
+        // Configure the cell...
+
+        return cell
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
